2001f658719a56e00b5a622a3db621388b1ebdaf
Brigham Andersen <brighamband@gmail.com>
2022-01-05 00:34:46 -0700

    mostly working!

--- script.py
+++ script.py
@@ -48,81 +48,92 @@ sleep(25)
 
 num_applies = 0  # Keeps track of how many times you applied successfully
 
+jobs_applied = []  # Array containing jobs where you successfully applied
+
 # Find all postings
 postings = driver.find_elements(By.XPATH, "//a[@data-hook='jobs-card']")
 
 # For each posting
 for posting in postings:
-    posting_name = driver.find_element(
-        By.XPATH,
-        '//*[@id="skip-to-content"]/div[3]/div/div[1]/div/form/div[2]/div/div/div[2]/div[1]/div[1]/h1/a',
-    ).text
-    posting_company = driver.find_element(
-        By.XPATH,
-        '//*[@id="skip-to-content"]/div[3]/div/div[1]/div/form/div[2]/div/div/div[2]/div[1]/div[1]/div[2]/div/div[2]/a',
-    ).text
-    print(posting_name, "@", posting_company)
-
     # Click on sidebar posting
     posting.click()
     sleep(2)
 
+    # Grab name info
+    posting_name = "?"
+    posting_company = "?"
+    posting_name_results = driver.find_elements(
+        By.XPATH,
+        '//*[@id="skip-to-content"]/div[3]/div/div[1]/div/form/div[2]/div/div/div[2]/div[1]/div[1]/h1/a',
+    )
+    if len(posting_name_results) == 1:
+        posting_name = posting_name_results[0].text
+    posting_company_results = driver.find_elements(
+        By.XPATH,
+        '//*[@id="skip-to-content"]/div[3]/div/div[1]/div/form/div[2]/div/div/div[2]/div[1]/div[1]/div[2]/div/div[2]/a',
+    )
+    if len(posting_company_results) == 1:
+        posting_company = posting_company_results[0].text
+    posting_headline = posting_name + " @ ", posting_company
+    print(posting_headline)
+
     # Check if there's a 'Quick Apply' button
-    apply_button_results = driver.find_elements(
+    apply_btn_results = driver.find_elements(
         By.XPATH, '//button/span/div[text()="Quick Apply"]'
     )
-    if len(apply_button_results) == 0:  # Check for normal apply if no luck
-        apply_button_results = driver.find_elements(
+    has_quick_apply = True if len(apply_btn_results) > 1 else False
+
+    if len(apply_btn_results) == 0:  # Check for normal apply if no luck
+        apply_btn_results = driver.find_elements(
             By.XPATH, '//button/span/div[text()="Apply"]'
         )
 
     # If there is one
-    if len(apply_button_results) > 1:
-
+    if len(apply_btn_results) > 1:
         # Click Apply Button
 
-        apply_button = apply_button_results[0]  # Grab the button from the list
-        apply_button.click()
+        apply_btn = apply_btn_results[0]  # Grab the button from the list
+        apply_btn.click()
+        sleep(2)
         exit_posting_btn = (
             driver.find_element(  # Get exit number in case you need to get out
                 By.XPATH, "/html/body/reach-portal/div[3]/div/div/div/span/div/button"
             )
         )
-        sleep(2)
 
-        # Click resume button
-
-        add_resume_btn_results = driver.find_elements(
-            By.XPATH,
-            "/html/body/reach-portal/div[3]/div/div/div/span/form/div[1]/div/div[2]/fieldset/div/div[2]/span[1]/button",
-        )
-        # If there's no add resume button, skip over that posting
-        if len(add_resume_btn_results) == 0:
-            exit_posting_btn.click()
-            continue  # Skip to next posting
-        add_resume_btn = add_resume_btn_results[0]  # Grab the button from the list
-        add_resume_btn.click()
-        sleep(2)
+        # Click resume button (If it's not Quick Apply.  If it is quick apply, jump straight to submitting.)
+        if not has_quick_apply:
+            add_resume_btn_results = driver.find_elements(
+                By.XPATH,
+                "/html/body/reach-portal/div[3]/div/div/div/span/form/div[1]/div/div[2]/fieldset/div/div[2]/span[1]/button",
+            )
+            # If there's no add resume button, skip over that posting
+            if len(add_resume_btn_results) == 0:
+                exit_posting_btn.click()
+                continue  # Skip to next posting
+            add_resume_btn = add_resume_btn_results[0]  # Grab the button from the list
+            add_resume_btn.click()
+            sleep(2)
 
         # Click 'Submit' Button
 
         submit_btn_results = driver.find_elements(
-            By.XPATH, '//span/div/button[text()="Submit Application"]'
+            By.XPATH, '//span/div/button/span[text()="Submit Application"]'
         )
         if len(submit_btn_results) == 0:
             exit_posting_btn.click()
             continue
         submit_btn = submit_btn_results[0]
-        # submit_btn = driver.find_element(
-        #     By.XPATH,
-        #     "/html/body/reach-portal/div[3]/div/div/div/span/form/div[2]/div/span/div/button",
-        # )
         submit_btn.click()
         sleep(2)
 
-        # Increment number of successful applies
-        num_applies += 1
+        # Success cleanup
 
+        jobs_applied.append(posting_headline)
+        num_applies += 1  # Increment number of successful applies
+
+print()
+print("Done!")
 # Close browser
 driver.close()
 
@@ -132,5 +143,7 @@ print(
     raw_job_query,
     "jobs!",
 )
+for job in jobs_applied:
+    print(job)
 APP_RESULTS_URL = "https://byu.joinhandshake.com/applications?ref=account-dropdown"
 print(f"Visit {APP_RESULTS_URL} for details on where you applied.")
