9a255d5d6bdbe1bd4d07e6008e24f9a8cd3569d4
Brigham Andersen <brigham.andersen@jolt.com>
2022-08-03 19:33:48 -0600

    Last push with basic form

--- app/app.py
+++ app/app.py
@@ -1,12 +1,27 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
+app.debug = True
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/apply", methods=["POST"])
+def apply():
+    username = request.form["username"]
+    password = request.form["password"]
+    query = request.form["query"]
+    return None
+
+
+@app.route("/done")
+def done():
+    return render_template("done.html")
 
 
 @app.route("/hello/<name>")
@@ -17,3 +32,7 @@ def hello(name):
     if name is None:
         return "<h1>Make sure to provide a name in the URL</h1>"
     return render_template("hello.html", name=name)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
